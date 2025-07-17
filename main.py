05a887757c6ec80006d57c5c1de0e44881289a9f
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-07-17 21:36:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,3 +58,9 @@ async def delete_user(email: str):
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="User not found")
     return {"message": "User deleted"}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
