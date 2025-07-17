68da8cb05b1a43ca51a739caf1bffcea790636c8
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-07-17 22:58:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-# from dotenv import load_dotenv
 import os
 
-# load_dotenv()
 MONGO_URL = os.getenv("MONGO_URI")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["user_db"]
