19de316c6eda4027285959f19406f6c415f83934
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-07-23 07:48:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://paradox-point.vercel.app/"
+        "https://paradox-point.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
